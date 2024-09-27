6e4e201bb598b15a43f4a3c68dfeba8508ae4894
ghanshyam kumar <133628810+ghanshyam9263@users.noreply.github.com>
2024-09-27 15:50:05 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+    void main(){
+    	int a,b;
+    	printf("inte enter value a:");
+    	scanf("%d", &a);
+    	printf("inter secand value b:");
+    	scanf("%d", &b);
+    	a=a+b;
+    	b=a-b;
+    	a=a-b;
+     	printf(" after swaping a: %d \n", a);
+    	printf("after swaping b : %d", b);
+    	
+    	
+    
+    	
+	}
---
+++ swap1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+    void main(){
+    	int a,b,c;
+    	printf("inte enter value a:");
+    	scanf("%d", &a);
+    	printf("inter secand value b:");
+    	scanf("%d", &b);
+    	  c=a;
+    	  a=b;
+    	  b=c;
+     	printf(" after swaping a: %d", a);
+    	printf("after swaping b : %d", b);
+    	
+    	
+    
+    	
+	}
